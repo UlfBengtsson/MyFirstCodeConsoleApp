e5387419d00a53c26d8f6f730fd50ebd3fa645d0
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-02-15 11:54:43 +0100

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        //Here is were the program starts
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\nI love to code");//Just text, this will not bet treated as code - Code note
+        }
+    }
+}
